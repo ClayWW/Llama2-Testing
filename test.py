ed262e4f865cf8a80fb6fce65885de39f64f4e6f
Clay <clayw@udel.edu>
2023-11-27 11:48:43 -0500

    Added qdrant pseudocode

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
 # Importing necessary modules and classes
 from transformers import AutoTokenizer  # Import AutoTokenizer for tokenizing input text
-import transformers  # Import the transformers library
-import torch  # Import the PyTorch library
+import transformers  # Import the transformers library for using its functionalities
+import torch  # Import the PyTorch library for deep learning functionalities
+from qdrant_client import QdrantClient  # Import QdrantClient for interacting with Qdrant database
+from qdrant_client.http.models import Filter  # Import Filter for filtering queries in Qdrant
+
+'''
+# These are placeholder function definitions for converting text to embeddings and vice versa
+def text_to_embedding():
+    # Function to convert text to embeddings; implementation depends on the chosen embedding method
+
+def embedding_to_text():
+    # Function to convert embeddings back to text; necessary if you store and retrieve embeddings in Qdrant
+'''
 
 # Define the model to be used
 model = "meta-llama/LLama-2-7b-chat-hf"  # Specify the Llama2 model available on Hugging Face
@@ -13,18 +24,39 @@ tokenizer = AutoTokenizer.from_pretrained(model)  # Initialize the tokenizer spe
 pipeline = transformers.pipeline(
     "text-generation",  # Specify the task as text generation
     model=model,  # Use the specified Llama2 model
-    torch_dtype=torch.float16,  # Set the data type for PyTorch tensors to float16 (reduced precision for efficiency)
+    torch_dtype=torch.float16,  # Set the data type for PyTorch tensors to float16 for efficiency
     device_map="auto",  # Automatically use available device (CPU or GPU) for model computation
 )
+
+# Initialize the Qdrant client (commented out)
+# qdrant_client = QdrantClient(host='localhost', port=6333)  # Set up Qdrant client with host and port
+
+# Define the input prompt (commented out)
+# prompt = 'I liked "Breaking Bad" and "Band of Brothers". Do you have any recommendations of other shows I might like?\n'
+# Convert the prompt to an embedding (commented out)
+# prompt_embedding = 
+
+'''
+# This section is for querying the Qdrant database with the generated prompt embedding
+response = qdrant_client.search(
+    collection_name="your_collection_name",  # Specify the collection name in Qdrant to search in
+    query_vector=prompt_embedding,  # Use the embedding of the prompt for the query
+    query_filter=Filter(),  # Optionally apply filters to the query
+    top=10  # Retrieve the top 10 similar results
+)
+
+# Process the response from Qdrant to convert embeddings back to text
+similar_texts = [embedding_to_text(item.embedding) for item in response.result.hits]
+'''
 
 # Generate sequences based on the input prompt
 sequences = pipeline(
     'I liked "Breaking Bad" and "Band of Brothers". Do you have any recommendations of other shows I might like?\n',
     do_sample=True,  # Enable random sampling for generating text
     top_k=10,  # Limit the number of highest probability vocabulary tokens to consider for each step
-    num_return_sequences=1,  # Return only one sequence
-    eos_token_id=tokenizer.eos_token_id,  # Specify the end-of-sequence token ID for stopping generation
-    max_length=200,  # Maximum length of the generated sequences
+    num_return_sequences=1,  # Specify that only one sequence should be returned
+    eos_token_id=tokenizer.eos_token_id,  # Use the end-of-sequence token ID to signal completion
+    max_length=200,  # Set the maximum length for the generated text
 )
 
 # Print the generated sequences
